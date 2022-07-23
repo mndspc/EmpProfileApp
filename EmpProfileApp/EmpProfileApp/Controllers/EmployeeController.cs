536b6973082b5bf4c279015b9a10b050589145db
Shoaib <shoaib.ghachi@gmail.com>
2022-07-23 18:37:25 +0530

    initial project commit

---
+++ EmpProfileApp/EmpProfileApp/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmpProfileApp.Models;
+namespace EmpProfileApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        static List<EmpProfile> empProfiles = new List<EmpProfile>
+        {
+            new EmpProfile{EmpCode=100,EmpName="Scott",Email="scott@gmail.com" },
+            new EmpProfile{EmpCode=101,EmpName="Tiger",Email="tiger@gmail.com"},
+            new EmpProfile{EmpCode=102,EmpName="Smith",Email="smith@gmail.com" },
+            new EmpProfile{EmpCode=103,EmpName="Adam",Email="adam@gmail.com"}
+        };
+        public ActionResult EmployeeList()
+        {
+            return View(empProfiles);
+        }
+    }
+}
---
+++ EmpProfileApp/EmpProfileApp/Models/EmpProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmpProfileApp.Models
+{
+    public class EmpProfile
+    {
+        public int EmpCode { get; set; }
+        public string EmpName { get; set; }
+
+        public string Email { get; set; }
+    }
+}
